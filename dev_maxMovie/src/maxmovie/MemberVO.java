652fdb6bf928b096489a2db74c42309cf02ca8ec
MiKoong <skymikong@gmail.com>
2020-03-26 22:22:06 +0900

    [2020-03-26 22:21]미경 VO수정 - MemberVO 추가 - TicketingVO 수정

---
+++ dev_maxMovie/src/maxmovie/MemberVO.java
@@ -0,0 +1,61 @@
+package maxmovie;
+
+public class MemberVO {
+	String command	    = null;
+	String mem_name     = null;
+	String mem_id       = null;
+	String mem_pw       = null;
+	String mem_email    = null;
+	String mem_nickname = null;
+	String mem_birth    = null;
+	String mem_gender   = null;
+	public String getMem_name() {
+		return mem_name;
+	}
+	public void setMem_name(String mem_name) {
+		this.mem_name = mem_name;
+	}
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	public String getMem_pw() {
+		return mem_pw;
+	}
+	public void setMem_pw(String mem_pw) {
+		this.mem_pw = mem_pw;
+	}
+	public String getMem_email() {
+		return mem_email;
+	}
+	public void setMem_email(String mem_email) {
+		this.mem_email = mem_email;
+	}
+	public String getMem_nickname() {
+		return mem_nickname;
+	}
+	public void setMem_nickname(String mem_nickname) {
+		this.mem_nickname = mem_nickname;
+	}
+	public String getMem_birth() {
+		return mem_birth;
+	}
+	public void setMem_birth(String mem_birth) {
+		this.mem_birth = mem_birth;
+	}
+	public String getMem_gender() {
+		return mem_gender;
+	}
+	public void setMem_gender(String mem_gender) {
+		this.mem_gender = mem_gender;
+	}
+	public String getCommand() {
+		return command;
+	}
+	public void setCommand(String command) {
+		this.command = command;
+	}
+	
+}
