08b1d2a64aa521e7ae1251b20e819bc801ebc72f
jhleelego <jhleelego@naver.com>
2020-04-06 21:15:55 +0900

    테스트

--- dev_maxMovie/src/maxmovie/ClientThread.java
+++ dev_maxMovie/src/maxmovie/ClientThread.java
@@ -3,30 +3,25 @@ package maxmovie;
 import java.util.StringTokenizer;
 
 public class ClientThread extends Thread{
-	
 	MaxMovieView mmv = null;
-	
 	boolean stop = false;
-
 	public ClientThread(MaxMovieView mmv) {
 		this.mmv = mmv;
 	}
 	//화면전환 할 때 사용하는 제어 메소드
-		public void display(boolean lv, boolean mrv, boolean mcv, boolean scv, boolean pv,
-							boolean mv, boolean thv, boolean miv, boolean muv, boolean rv) {
-			mmv.jp_lv.setVisible(lv);
-			mmv.jp_mrv.setVisible(mrv);
-			mmv.jp_mrv.jp_mcv.setVisible(mcv);
-			mmv.jp_mrv.jp_scv.setVisible(scv);
-			mmv.jp_mrv.jp_pv.setVisible(pv);
-			mmv.jp_mv.setVisible(mv);
-			mmv.jp_mv.jp_thv.setVisible(thv);
-			mmv.jp_mv.jp_miv.setVisible(miv);
-			mmv.jp_mv.jp_muv.setVisible(muv);
-			mmv.jp_rv.setVisible(rv);
-		}
-	
-	
+	public void display(boolean lv, boolean mrv, boolean mcv, boolean scv, boolean pv,
+						boolean mv, boolean thv, boolean miv, boolean muv, boolean rv) {
+		mmv.jp_lv.setVisible(lv);
+		mmv.jp_mrv.setVisible(mrv);
+		mmv.jp_mrv.jp_mcv.setVisible(mcv);
+		mmv.jp_mrv.jp_scv.setVisible(scv);
+		mmv.jp_mrv.jp_pv.setVisible(pv);
+		mmv.jp_mv.setVisible(mv);
+		mmv.jp_mv.jp_thv.setVisible(thv);
+		mmv.jp_mv.jp_miv.setVisible(miv);
+		mmv.jp_mv.jp_muv.setVisible(muv);
+		mmv.jp_rv.setVisible(rv);
+	}
 	//화면전환 할 때 사용하는 제어 메소드
 	public void display(boolean lv, boolean mv, 
 							boolean miv,boolean muv, boolean thv, 
